baedb567fd58898e2dac806f5203f0f70982af8c
Martin Opstad Reistadbakk <martin@reistadbakk.com>
2012-02-23 23:17:54 +0100

    initial oauth

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ end
 
 gem 'jquery-rails'
 
+gem 'oauth'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+require 'oauth/consumer'
+
+class HomeController < ApplicationController
+
+  def index
+    @consumer=OAuth::Consumer.new( "UB1afSZlv5MIDwyFWNMoLQ","zqxA10TfL55N60tG2G8FeK19DOKbCWTrBRcLOPoDaI", {
+        :site=>"https://api.twitter.com/"
+        })
+    @request_token=@consumer.get_request_token
+    redirect_to @request_token.authorize_url
+    
+  end
+  
+  def callback
+    @access_token=@request_token.get_access_token
+  end
+end
