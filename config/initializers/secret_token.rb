7629cf8c0f8b63347625cb5d50f109a0c1a79fea
Martin Opstad Reistadbakk <martin@reistadbakk.com>
2012-02-23 22:26:39 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weed::Application.config.secret_token = '172bdb147999db0f62632f04856f54ccb4a94cfb6228e6a7dab57e451b971ef0b7d3f992d85611ed98b86f8b3299af43e440043fc653696246996e9d4a170353'
