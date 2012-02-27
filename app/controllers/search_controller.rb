bb3a8b8c618d30c61c76071694ffbc5ebf3a3188
Martin Opstad Reistadbakk <martin@reistadbakk.com>
2012-02-27 14:58:14 +0100

    added search controller

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,15 @@
+require 'oauth_twitter'
+
+class SearchController < ApplicationController
+  include OauthTwitter
+  def index
+    @twitter_user = verify_credentials
+    if @twitter_user.nil?
+      redirect_to :controller => 'application', :action => :index
+    end
+    @q = params[:q]
+    @new_friends = search_users(URI.escape(@q))
+    puts "search controller"
+    puts @new_friends.inspect
+  end
+end
