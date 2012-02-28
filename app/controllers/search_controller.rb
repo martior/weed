5194ff44eea81644f904a0374fb0e230fc87d58f
Martin Opstad Reistadbakk <martin@reistadbakk.com>
2012-02-28 20:21:06 +0100

    search and bulk follow works

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,40 +7,57 @@ class SearchController < ApplicationController
     if @twitter_user.nil?
       redirect_to :controller => 'application', :action => :index
     end
-    @q = params[:q]
-    @page = params[:page]
-    if @page.nil?
-      @page = 0
-    else
-      @page = @page.to_i
-    end
-    @page_max = params[:page_max]
-    if @page_max.nil?
-        @page_max = 0
-    else
-      @page_max= @page_max.to_i
-    end
-    if @page > 0
-      @previous = @page -1
-    else
-      @previous = 0
-    end
-    if @page < @page_max
-      @next = @page  + 1
-    else
-      @next = @page_max
-    end
-    if @page > @page_max
-      @page_max = @page
-    end
-    @new_friends = search_users(URI.escape(@q),@page)
-    if @new_friends.size >= 20
-      @more = true
-      if @next == @page_max
-        @next += 1
+    follow = params[:follow]
+    unless follow.nil?
+      follow.each do|new_friend|
+        create_friendship(new_friend)
       end
+      flash[:notice] = "Follow successfull"
+      redirect_to :back
     else
-      @more = false
+      @q = params[:q] 
+      @page = params[:page]
+      if @page.nil?
+        @page = 0
+      else
+        @page = @page.to_i
+      end
+      @page_max = params[:page_max]
+      if @page_max.nil?
+          @page_max = 0
+      else
+        @page_max= @page_max.to_i
+      end
+      if @page > @page_max
+        @page_max = @page
+      end
+      if @page > 0
+        @previous = @page -1
+      else
+        @previous = 0
+      end
+      if @page < @page_max
+        @next = @page  + 1
+      else
+        @next = @page_max
+      end
+      if @page > @page_max
+        @page_max = @page
+      end
+      if @q.nil?
+        @new_friends = []
+      else
+        @new_friends = search_users(URI.escape(@q),@page)
+      end
+
+      if @new_friends.size >= 20
+        @more = true
+        if @next == @page_max
+          @next += 1
+        end
+      else
+        @more = false
+      end
     end
   end
 end
